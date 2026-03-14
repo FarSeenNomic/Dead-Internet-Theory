6c4aaf5015217da80d72426b22ac7a8de8f53376
programstarter <programstarter@gmail.com>
2026-03-14 14:19:54 -0700

    Create account test

--- testcases/case001.py
+++ testcases/case001.py
@@ -1,6 +1,26 @@
+import requests
 # compiles and runs
 
 # API Create account
+
+def test_register_endpoint():
+    url = "http://localhost/register"
+
+    form_data = {
+        "username": "example_user",
+        "password": "ExamplePassword123!"
+    }
+
+    response = requests.post(url, data=form_data)
+
+    assert response.status_code == 200
+
+    data = response.json()
+    assert data["status"] == "success"
+
+    return data
+
+# API Login and get API key
 
 # API Make post
 
