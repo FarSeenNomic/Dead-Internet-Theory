ff6598134ffd3114027070daec963b41a8df8d0c
programstarter <programstarter@gmail.com>
2026-03-27 11:24:35 -0700

    Test cases for updating PFP, display name, and bio. No Verification

--- testcases/case001.py
+++ testcases/case001.py
@@ -24,21 +24,32 @@ try:
   assert response.status_code == 200
   print(f"registered {user1_data['username']}")
 except AssertionError:
-  print(response.status_code)
+  print("Error", response.status_code)
 
 try:
   response = user1.post("http://localhost:5000/login", data=user1_data)
   assert response.status_code == 200
   print(f"login {user1_data['username']}")
 except AssertionError:
-  print(response.status_code)
+  print("Error", response.status_code)
+
+try:
+  response = user1.post("http://localhost:5000/settings/profile", data={
+    "bio": "I wonder how a degenerated person like that could have reached a position of responsibility in the Army Medical Corps.",
+    "pfp": "https://64.media.tumblr.com/avatar_5562e40dd3bb_128.pnj",
+    "displayname": '"Hawkeye" Pierce',
+    })
+  assert response.status_code == 200
+  print(f"updated PFP {user1_data['username']}")
+except AssertionError:
+  print("Error", response.status_code)
 
 try:
   response = user1.post("http://localhost:5000/create", data={"text": "War isn't Hell. War is war, and Hell is Hell. And of the two, war is a lot worse. There are no innocent bystanders in Hell."})
   assert response.status_code == 200
   print(f"create {user1_data['username']}")
 except AssertionError:
-  print(response.status_code)
+  print("Error", response.status_code)
 
 #Text username in use
 #Test password minimum length
@@ -46,7 +57,12 @@ except AssertionError:
 
 user2.post("http://localhost:5000/register", data=user2_data)
 user2.post("http://localhost:5000/login", data=user2_data)
-print(f"register and login {user2_data['username']}")
+user2.post("http://localhost:5000/settings/profile", data={
+  "bio": "I'm a temporarily misassigned civilian.",
+  "pfp": "https://pbs.twimg.com/media/GF8fsetXQAAellr.jpg",
+  "displayname": 'Captain B.J. Hunnicutt',
+  })
+print(f"register, login, and update {user2_data['username']}")
 
 try:
   response = user3.post("http://localhost:5000/register", data=user1_data)
@@ -65,19 +81,18 @@ try:
   assert response.status_code == 200
   print(f"{user3_data['username']} tried to make a user under the name {user2_data['username']}")
 except AssertionError:
-  print(response.status_code)
+  print("Error", response.status_code)
 
 user3.post("http://localhost:5000/register", data=user3_data)
 user3.post("http://localhost:5000/login", data=user3_data)
 print(f"register and login {user3_data['username']}")
-
 
 try:
   response = user2.get(f"http://localhost:5000/user.json/@{user1_data['username']}").json()
   print(f"Made {user2_data['username']} view all posts from {user1_data['username']}")
   print(response)
 except Exception as e:
-  print(e)
+  print("Error", e)
 
 post2like = response[0]["snowflake"]
 post2follow = response[0]["owner_snowflake"]
@@ -88,7 +103,7 @@ try:
   print(response.content)
   print(response.json())
 except Exception as e:
-  print(e)
+  print("Error", e)
 
 try:
   response = user2.post(f"http://localhost:5000/follow.json/{post2follow}")
@@ -96,25 +111,25 @@ try:
   print(response.content)
   print(response.json())
 except Exception as e:
-  print(e)
+  print("Error", e)
 
 try:
   response = user2.post(f"http://localhost:5000/reply/{post2like}", data={"text": "To the ends of the earth, friend."})
   print(f"Reply {user2_data['username']}")
   assert response.status_code == 200
 except AssertionError as e:
-  print(e)
+  print("Error", e)
 
 try:
   response = user3.post(f"http://localhost:5000/reply/{post2like}", data={"text": "Baah."})
   print(f"Reply {user3_data['username']}")
   assert response.status_code == 200
 except AssertionError as e:
-  print(e)
+  print("Error", e)
 
 
 #try:
 #  print("Rebooting!")
 #  response = user3.post(f"http://localhost:5000/reboot", data={"REBOOTCODE": "butts"})
 #except requests.exceptions.ConnectionError as e:
-#  print(e)
+#  print("Error", e)
