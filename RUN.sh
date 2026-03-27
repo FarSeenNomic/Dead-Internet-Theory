94af0d69ce333bc59f23efc9f684f2797a372cb9
programstarter <programstarter@gmail.com>
2026-03-27 12:24:24 -0700

    This frontend needs a different command

--- RUN.sh
+++ RUN.sh
@@ -8,7 +8,7 @@ while true; do
     git pull || echo "Git pull failed"
 
     echo "Starting Flask..."
-    python3 -m flask run --host 0.0.0.0 || echo "Flask exited"
+    python3 -m flask --app app:wib run --host 0.0.0.0 || echo "Flask exited"
 
     echo "Restarting in 3 seconds..."
     sleep 3
